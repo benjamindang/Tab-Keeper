c0c679049812b4f313258e68d1a69f4d82399e89
Benjamin Dang <benjamindang@s-169-232-88-227.resnet.ucla.edu>
2016-11-06 20:25:57 -0800

    Implemented working solution to save and open functions for all tabs, extension fully working

--- background.js
+++ background.js
@@ -1,4 +1,22 @@
-chrome.browserAction.onClicked.addListener(function(activeTab){
-  var newURL = "http://stackoverflow.com/";
-  chrome.tabs.create({ url: newURL });
-});
+// chrome.browserAction.onClicked.addListener(function(activeTab){
+//   var newURL = "http://stackoverflow.com/";
+//   chrome.tabs.create({ url: newURL });
+// });
+var tabUrls = [];
+function save() {
+	tabUrls = [];
+	chrome.windows.getAll({populate:true},function(windows){
+  		windows.forEach(function(window){
+    		window.tabs.forEach(function(tab){
+    			tabUrls.push(tab.url);
+    		});
+  		});
+	});
+}
+function open(){
+	for(var i = 0; i < tabUrls.length; i++){
+		chrome.tabs.create({url: tabUrls[i]});
+	}
+}
+document.getElementById("save").onclick = save;
+document.getElementById("open").onclick = open;
